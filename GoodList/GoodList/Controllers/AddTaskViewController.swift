00e59f6e37b13e11faedf5821a3f78e490ca1006
Adriaan van Schalkwyk <adriaan.vanschalkwyk@absa.africa>
2021-04-21 17:12:34 +0200

    add save new task screen

---
+++ GoodList/GoodList/Controllers/AddTaskViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddTaskViewController.swift
+//  GoodList
+//
+//  Created by Adriaan van Schalkwyk on 2021/04/21.
+//
+
+import Foundation
+import  UIKit
+
+class AddTaskViewController: UIViewController {
+    
+}
