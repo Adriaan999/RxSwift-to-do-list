8f36c8ef8394132b94bb20c2880932fe28891d0c
Adriaan van Schalkwyk <adriaan.vanschalkwyk@absa.africa>
2021-04-21 17:03:00 +0200

    add first screen

---
+++ GoodList/GoodList/Controllers/TaskListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TaskListViewController.swift
+//  GoodList
+//
+//  Created by Adriaan van Schalkwyk on 2021/04/21.
+//
+
+import Foundation
+import UIKit
+
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        return cell
+    }
+}
